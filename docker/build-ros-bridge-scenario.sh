e2cf554a72fef193aea224a58dc8e6dc23a417d1
lauri <lauri.a.suomela@tuni.fi>
2023-09-25 15:08:22 +0300

    Update visual_robot_localization, remove the gpg key update trick

--- docker/build-ros-bridge-scenario.sh
+++ docker/build-ros-bridge-scenario.sh
@@ -6,21 +6,16 @@ SCRIPT_DIR=$(dirname "${SCRIPT}")
 ROS_DISTRO="foxy"
 IMAGE_NAME="carla-vloc-benchmark:0.0.1"
 
-
+# Build carla-ros-bridge
 cd ${SCRIPT_DIR}/../third_party/ros-bridge/docker
-# https://discourse.ros.org/t/ros-gpg-key-expiration-incident/20669
-# Also, the ROS ppa has to be removed while curl is being installed
-sed -i '12 a RUN /bin/bash -c "mv /etc/apt/sources.list.d/ros2-latest.list /etc/ros2-latest.list"' Dockerfile
-sed -i '13 a RUN /bin/bash -c "apt-get update && apt-get install -y curl"' Dockerfile
-sed -i '14 a RUN /bin/bash -c "mv /etc/ros2-latest.list /etc/apt/sources.list.d/ros2-latest.list"' Dockerfile
-sed -i '15 a RUN /bin/bash -c "curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -; curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg"' Dockerfile
 ./build.sh
-sed -i '13,16d' Dockerfile
 
+# Build visual_robot_localization
 TEMP_IMAGE_NAME="carla_ros_bridge_vloc:0.0.1"
 cd ${SCRIPT_DIR}/../visual_robot_localization/docker
 ./build.sh -b "carla-ros-bridge:$ROS_DISTRO" -t $TEMP_IMAGE_NAME
 
+# Build the benchmark
 cd ${SCRIPT_DIR}
 docker build \
     --build-arg ROS_DISTRO=$ROS_DISTRO \
